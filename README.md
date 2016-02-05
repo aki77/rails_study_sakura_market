9f9375e16c45c9efd0aa185ed6ca41e6677b125f
aki <lala.akira@gmail.com>
2016-02-05 15:25:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rails Study Sakura Market
+
+## This application requires:
+- Ruby 2.3.0
+- Rails 4.2.5.1
---
+++ config/initializers/haml.rb
@@ -0,0 +1,3 @@
+# http://qiita.com/kami_zh/items/913a8cfd1ddc2052b1c8
+Haml::Template.options[:attr_wrapper] = '"'
+Haml::Template.options[:ugly] = true
